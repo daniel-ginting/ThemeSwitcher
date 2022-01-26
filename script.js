17c74f768bffdfc1f4f216275f04650970ba4ea0
Daniel Ginting <daniel_yjg@yahoo.com>
2022-01-26 11:09:57 +0700

    clean up codes

--- script.js
+++ script.js
@@ -14,26 +14,16 @@ function imageMode(color) {
     image3.src = `img/undraw_conceptual_idea_${color}.svg`;
 }
 
-// Dark Mode Styles
-
-function darkMode() {
-    nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
-    textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
-    toggleIcon.children[0].textContent = 'Dark Mode';
-    toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
-    toggleIcon.children[1].classList.add('fa-moon');
-    imageMode('dark')
-}
-
-// Light Mode Styles
-
-function lightMode() {
-    nav.style.backgroundColor = 'rgb(255 255 255 / 50%)';
-    textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
-    toggleIcon.children[0].textContent = 'Light Mode';
+function toggleDarkLightMode(isDark) {
+    nav.style.backgroundColor = isDark ? 'rgb(0 0 0 / 50%)' : 'rgb(255 255 255 / 50%)';
+    textBox.style.backgroundColor = isDark ? 'rgb(255 255 255 / 50%)' : 'rgb(0 0 0 / 50%)';
+    toggleIcon.children[0].textContent = isDark ? 'Dark Mode' : 'Light Mode';
+    isDark ? toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon'):
     toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
-    imageMode('light');
+    isDark?imageMode('dark'):imageMode('light');
 }
+
+
 
 // Switch Theme Dynamically
 
@@ -41,11 +31,11 @@ function switchTheme(event) {
     if (event.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
         localStorage.setItem('theme', 'dark')
-        darkMode();
+        toggleDarkLightMode(true);
     } else {
         document.documentElement.setAttribute('data-theme', 'light');
         localStorage.setItem('theme', 'light')
-        lightMode();
+        toggleDarkLightMode(false);
     }
 }
 
@@ -60,6 +50,6 @@ if (currentTheme) {
     document.documentElement.setAttribute('data-theme', currentTheme);
     if(currentTheme === 'dark') {
         toggleSwitch.checked = true;
-        darkMode();
+        toggleDarkLightMode(true);
     }
 }
